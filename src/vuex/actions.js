2ae9f547f02f3aa312309bd54cb2c01a4f473d63
yaoyuxiao <531416635@qq.com>
2018-04-05 22:35:39 +0800

    微信jssdk调试

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -1,6 +1,6 @@
-// import axios from '@/api/axios'
 import axios from 'axios'
-import {router} from '../router/index'
+import wx from 'weixin-js-sdk'
+
 /**
  * 解决跨域请求时，session丢失的问题（需与后台跨域请求一起设置Credentials）
  * @type {boolean}
@@ -32,6 +32,62 @@ const actions = {
     }).catch((err) =>{
     })
   },
+
+  //微信jssdk获取用户位置
+  getLocationWx({ commit,state},val){
+    let url = val.url
+    if("" == url || null == url || undefined == url){
+      return false;
+    }
+    axios({
+      method: 'post',
+      url: hostUrl + 'wechat/getJsSdkConfig.do',
+      data : {
+        url: url
+      },
+    }).then((response) => {
+      let res = response.data
+      console.log(res)
+      wx.config({
+        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        appId: res.appId, // 必填，公众号的唯一标识
+        timestamp: res.timestamp, // 必填，生成签名的时间戳
+        nonceStr: res.nonceStr, // 必填，生成签名的随机串
+        signature: res.signature, // 必填，签名，见附录1
+        jsApiList: ['getLocation']
+      })
+      wx.ready(() => {
+
+        wx.getLocation({
+          type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+          success: function (res) {
+            var latitude = res.latitude // 纬度，浮点数，范围为90 ~ -90
+            var longitude = res.longitude // 经度，浮点数，范围为180 ~ -180。
+            var speed = res.speed // 速度，以米/每秒计
+            var accuracy = res.accuracy // 位置精度
+
+            // 地址解析:http://lbs.qq.com/javascript_v2/guide-service.html#link-four
+            let geocoder = new qq.maps.Geocoder({
+              complete: function (result) {
+                resolve(result.detail.address)
+              }
+            })
+            var coord = new qq.maps.LatLng(res.latitude, res.longitude)
+            let add = geocoder.getAddress(coord)
+            alert(add)
+          }
+        })
+
+      })
+
+      this.$wechat.error(function(res){
+        // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+        console.log("error=========")
+        console.log(res)
+      });
+    }).catch((err) =>{
+    })
+  }
 
 }
 
