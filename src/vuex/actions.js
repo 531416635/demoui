184228ed0dbf0c76749ed4f5cec38e8f51a8b580
yaoyuxiao <531416635@qq.com>
2018-04-06 19:14:38 +0800

    更换网址

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -7,7 +7,7 @@ import wx from 'weixin-js-sdk'
  */
 axios.defaults.withCredentials = true;
 
-const hostUrl = "http://47.94.99.53/demo0307/"
+const hostUrl = "http://www.myyunfu.com/demo0307/"
 //图片大小限制 5M的大小为 5*1024*1024 即 1048576*5
 var imgSize = 5242880;
 
@@ -26,7 +26,7 @@ const actions = {
       if (code == 1 && msgCode == 1) {
         commit('setUserInfo', result)
       } else {
-        let url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxec4ae298ee5742d7&redirect_uri=http%3a%2f%2f47.94.99.53%2fdemo0307%2fwechat%2fgetAuth.do&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect'
+        let url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxec4ae298ee5742d7&redirect_uri=http%3a%2f%2fwww.myyunfu.com%2fdemo0307%2fwechat%2fgetAuth.do&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect'
         window.location.href = url
       }
     }).catch((err) => {
